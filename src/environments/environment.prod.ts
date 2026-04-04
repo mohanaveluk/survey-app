3c37ad5af838252506cd5382067221dfb0aa512c
Mohanavelu <mohanaveluk@gmail.com>
2026-04-03 21:18:40 -0500

    update api production url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://survey-api-886135112987.us-central1.run.app/api'
+    //apiUrl: 'https://survey-api-886135112987.us-central1.run.app/api'
+    apiUrl: 'api.voter-pulse.com/api'
   };
